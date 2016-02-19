cuckoosandbox/cuckoo

commit 8fcad5c42ac278cf714021bf00e2bbfb2253e48d

improve random timeouts when netlog connects back
Every now and then it would happen that for some reason the initial
socket connection would fail and then, as no timeout was provided, it
could take quite a bit before the connection completed - hanging the
entire analysis. Sometimes three seconds but sometimes up to a minute.

By providing a timeout of 0.1 seconds we actively force the socket to
try to connect, generally connecting successfully after the first,
second, or at most a third try.

An example log file of a situation where the initial connect attempt
failed and no timeout was given looks roughly as follow:

2015-07-25 13:18:57,022 [lib.cuckoo.core.guest] DEBUG: Uploading analyzer to guest (id=cuckoo1, ip=192.168.56.102)
2015-07-25 13:18:57,279 [lib.cuckoo.core.guest] DEBUG: cuckoo1: analyzer started with PID 2192
2015-07-25 13:18:57,280 [lib.cuckoo.core.guest] DEBUG: cuckoo1: waiting for completion
2015-07-25 13:18:58,281 [lib.cuckoo.core.guest] DEBUG: cuckoo1: analysis not completed yet (status=2)
2015-07-25 13:18:59,284 [lib.cuckoo.core.guest] DEBUG: cuckoo1: analysis not completed yet (status=2)
2015-07-25 13:19:00,287 [lib.cuckoo.core.guest] DEBUG: cuckoo1: analysis not completed yet (status=2)
2015-07-25 13:19:01,289 [lib.cuckoo.core.guest] DEBUG: cuckoo1: analysis not completed yet (status=2)
2015-07-25 13:19:02,292 [lib.cuckoo.core.guest] DEBUG: cuckoo1: analysis not completed yet (status=2)

Followed by the following message after the connection was successful.
After this patch the following line should show up pretty much instantly
in every analysis.

2015-07-25 13:20:06,453 [lib.cuckoo.core.resultserver] DEBUG: LogHandler for live analysis.log initialized.

analyzer/windows/lib/common/results.py
@@ -4,7 +4,6 @@
 
 import logging
 import socket
-import time
 
 from lib.core.config import Config
 
@@ -38,22 +37,20 @@ class NetlogConnection(object):
         self.proto = proto
 
     def connect(self):
-        i = 1
-        # this can loop forever, if we can't connect the whole analysis is useless anyways
-        while True:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Try to connect as quickly as possible. Just sort of force it to
+        # connect with a short timeout.
+        while not self.sock:
             try:
-                s.connect((self.hostip, self.hostport))
+                s = socket.create_connection((self.hostip, self.hostport), 0.1)
                 s.sendall(self.proto)
-            except:
-                time.sleep(i)
-                i = min(i + 1, 60)
-            else:
-                self.sock = s
-                break
+            except socket.error:
+                continue
+
+            self.sock = s
 
     def send(self, data, retry=True):
-        if not self.sock: self.connect()
+        if not self.sock:
+            self.connect()
 
         try:
             self.sock.sendall(data)

northshade/cuckoo-modified

commit 0f7d524be55fdbc3adef2b2d145fb7863105d006

Fix a very old bug introduced by: https://github.com/cuckoosandbox/cuckoo/commit/8fcad5c42ac278cf714021bf00e2bbfb2253e48d
As the timeout applies not just to the initial connect but to any
operation occurring on the socket thereafter, it was observed especially
with large files that the timeout would be hit and the upload prematurely
and silently truncated.  Now all uploads will proceed correctly up to the
maximum allowed size.

analyzer/windows/lib/common/results.py
@@ -44,12 +44,13 @@ class NetlogConnection(object):
         while not self.sock:
             try:
                 s = socket.create_connection((self.hostip, self.hostport), 0.1)
-                s.sendall(self.proto)
             except socket.error:
                 time.sleep(0.1)
                 continue
 
             self.sock = s
+            self.sock.settimeout(None)
+            self.sock.sendall(self.proto)
             self.connected = True
 
     def send(self, data, retry=True):
@@ -61,17 +62,10 @@ class NetlogConnection(object):
         totalsent = 0
 
         while totalsent < len(data):
-            try:
-                cursent = self.sock.send(data[totalsent:])
-                if cursent == 0:
-                    raise socket.error
-                totalsent += cursent
-            except Exception as e:
-                # We really have nowhere to log this, if the netlog connection
-                # does not work, we can assume that any logging won't work either.
-                # So we just fail silently.
-                self.close()
-                break
+            cursent = self.sock.send(data[totalsent:])
+            if cursent == 0:
+                raise socket.error
+            totalsent += cursent
 
     def close(self):
         try: