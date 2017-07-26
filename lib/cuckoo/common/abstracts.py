e2416d878903bec14bdec19ffd9a93043863b999
doomedraven <doomedraven@users.noreply.github.com>
2017-07-26 18:50:23 +0200

    Update abstracts.py

--- lib/cuckoo/common/abstracts.py
+++ lib/cuckoo/common/abstracts.py
@@ -691,7 +691,6 @@ class Signature(object):
     filter_apinames = set()
     filter_categories = set()
     filter_analysistypes = set()
-    _alexadb = self._load_alexadb()
     
     def __init__(self, results=None):
         self.data = []
@@ -702,6 +701,10 @@ class Signature(object):
         self._current_call_raw_cache = None
         self._current_call_raw_dict = None
         
+        if not hasattr(Signature, "_alexadb"):
+            # initialize only once
+            Signature._alexadb = self._load_alexadb()
+            
     def _load_alexadb(self):
         alexa = dict()
         alexa_path = os.path.join(CUCKOO_ROOT, 'data', 'alexa.csv')
