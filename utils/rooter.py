24d08f440a01de9555a85f3cc2ac0e8dd61a452f
doomedraven <doomedraven@users.noreply.github.com>
2017-07-26 13:23:49 +0200

    add missed drop_(enable|disable)

--- utils/rooter.py
+++ utils/rooter.py
@@ -187,6 +187,25 @@ def tor_disable(ipaddr, resultserver_port, dns_port, proxy_port):
        dns_port)
    run(settings.iptables, "-D", "OUTPUT", "-s", ipaddr, "-j", "DROP")
 
+def drop_enable(ipaddr, resultserver_port):
+  run(settings.iptables, "-t", "nat", "-I", "PREROUTING", "--source", ipaddr,
+      "-p", "tcp", "--syn", "--dport", resultserver_port, "-j", "ACCEPT")
+  run(settings.iptables, "-A", "INPUT", "--destination", ipaddr, "-p", "tcp", "--dport", "8000", "-j", "ACCEPT")
+  run(settings.iptables, "-A", "INPUT", "--destination", ipaddr, "-p", "tcp", "--sport", resultserver_port, "-j", "ACCEPT")
+  run(settings.iptables, "-A", "OUTPUT", "--destination", ipaddr, "-p", "tcp",  "--dport", "8000", "-j", "ACCEPT")
+  run(settings.iptables, "-A", "OUTPUT", "--destination", ipaddr, "-p", "tcp",  "--sport", resultserver_port, "-j", "ACCEPT")
+  run(settings.iptables, "-A", "OUTPUT", "--destination", ipaddr, "-j", "DROP")
+
+def drop_disable(ipaddr, resultserver_port):
+  run(settings.iptables , "-t", "nat", "-D", "PREROUTING", "--source", ipaddr,
+      "-p", "tcp", "--syn", "--dport", resultserver_port, "-j", "ACCEPT")
+  run(settings.iptables, "-D", "INPUT", "--destination", ipaddr, "-p", "tcp", "--dport", "8000", "-j", "ACCEPT")
+  run(settings.iptables, "-D", "INPUT", "--destination", ipaddr, "-p", "tcp", "--sport", resultserver_port, "-j", "ACCEPT")
+  run(settings.iptables, "-D", "OUTPUT", "--destination", ipaddr, "-p", "tcp", "--dport", "8000", "-j", "ACCEPT")
+  run(settings.iptables, "-D", "OUTPUT", "--destination", ipaddr, "-p", "tcp",  "--sport", resultserver_port, "-j", "ACCEPT")
+  run(settings.iptables, "-D", "OUTPUT", "--destination", ipaddr, "-j", "DROP")
+
+
 handlers = {
     "nic_available": nic_available,
     "rt_available": rt_available,
@@ -206,6 +225,8 @@ handlers = {
     "inetsim_disable": inetsim_disable,
     "tor_enable": tor_enable,
     "tor_disable": tor_disable,
+    "drop_enable": drop_enable,
+    "drop_disable": drop_disable,
 }
 
 if __name__ == "__main__":
@@ -311,4 +332,3 @@ if __name__ == "__main__":
                 "output": output,
                 "exception": str(e) if e else None,
             }), addr)
-
